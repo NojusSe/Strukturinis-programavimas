be95c81038a83438805046672ab679966b664a18
NojusSe <nojus.semeklis@stud.viko.lt>
2024-10-03 23:42:33 +0300

    Pridejau koda i programa

--- Praktinis1/cmake-build-debug/cmake_install.cmake
+++ Praktinis1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Nojus/Documents/StrukturinisProgramavimas/Praktinis
+# Install script for directory: C:/Users/Nojus/Documents/Strukturinis-programavimas/Praktinis1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Nojus/Documents/StrukturinisProgramavimas/Praktinis/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Nojus/Documents/Strukturinis-programavimas/Praktinis1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Praktinis1/main.cpp
+++ Praktinis1/main.cpp
@@ -1,24 +1,90 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-double GBPBendras = 0.8593;
-double GBPPirkti = 0.8450;
-double GBPParduoti = 0.9060;
+const double GBP_Bendras = 0.8593;
+const double GBP_Pirkti = 0.8450;
+const double GBP_Parduoti = 0.9060;
 
-double USDBendras = 1.0713;
-double USDPirkti = 1.0547;
-double USDParduoti = 1.1309;
+const double USD_Bendras = 1.0713;
+const double USD_Pirkti = 1.0547;
+const double USD_Parduoti = 1.1309;
 
-double INRBendras = 88.8260;
-double INRPirkti = 85.2614;
-double INRParduoti = 92.8334;
+const double INR_Bendras = 88.8260;
+const double INR_Pirkti = 85.2614;
+const double INR_Parduoti = 92.8334;
 
-void ValiutosKeitimas();
+void palygintiValiuta(string valiuta) {
+    double kaina = 0.0;
+
+    if (valiuta == "GBP") kaina = GBP_Bendras;
+    else if (valiuta == "USD") kaina = USD_Bendras;
+    else if (valiuta == "INR") kaina = INR_Bendras;
+    else {
+        cout << "Netinkama valiuta" << endl;
+        return;
+    }
+    cout << "1 EUR = " << setprecision(4) << kaina << " " << valiuta << endl;
+}
+
+void pirktiValiuta(string valiuta, double kiekis) {
+    double kaina = 0.0;
+
+    if (valiuta == "GBP") kaina = GBP_Pirkti;
+    else if (valiuta == "USD") kaina = USD_Pirkti;
+    else if (valiuta == "INR") kaina = INR_Pirkti;
+    else {
+        cout << "Netinkama valiuta" << endl;
+        return;
+    }
+    double rezultatas = kiekis * kaina;
+    cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " " << valiuta << " uz " << kiekis << " EUR" << endl;
+}
+
+void parduotiValiuta(string valiuta, double kiekis) {
+    double kaina = 0.0;
+
+    if (valiuta == "GBP") kaina = GBP_Parduoti;
+    else if (valiuta == "USD") kaina = USD_Parduoti;
+    else if (valiuta == "INR") kaina = INR_Parduoti;
+    else {
+        cout << "Netinkama valiuta" << endl;
+        return;
+    }
+    double rezultatas = kiekis / kaina;
+    cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " EUR uz " << kiekis << " " << valiuta << endl;
+}
 
 int main() {
-    double valiuta = 0.0;
+    int pasirinkimas;
+    string valiuta;
+    double kiekis;
+
+    cout << "Valiutu keitimas" << endl;
+    cout << "Pasirinkite veiksma kuri norite daryti:" << endl;
+    cout << "1. Palyginti valiuta" << endl;
+    cout << "2. Pirkti valiuta" << endl;
+    cout << "3. Parduoti valiuta" << endl;
+    cin >> pasirinkimas;
+
+    cout << "Iveskite valiuta (GBP, USD, INR): ";
+    cin >> valiuta;
+
+    if (pasirinkimas == 1) {
+        palygintiValiuta(valiuta);
+    }
+    else if (pasirinkimas == 2) {
+        cout << "Iveskite kieki EUR valiutos: ";
+        cin >> kiekis;
+        pirktiValiuta(valiuta, kiekis);
+    }
+    else if (pasirinkimas == 3) {
+        cout << "Iveskite kieki EUR valiutos: ";
+        cin >> kiekis;
+        parduotiValiuta(valiuta, kiekis);
+    }
+    else {
+        cout << "Neteisinga" << endl;
+    }
     return 0;
 }
-void ValiutosKeitimas();
-    double valiuta = 0.0;
-    if(valiuta == )
