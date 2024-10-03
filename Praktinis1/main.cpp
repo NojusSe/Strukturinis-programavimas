c8a5bf6ed2798b64bb02f79fc2fd3b9929838d1f
NojusSe <nojus.semeklis@stud.viko.lt>
2024-10-03 15:07:10 +0300

    pridejau pirma praktini darba

---
+++ Praktinis1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+double GBPBendras = 0.8593;
+double GBPPirkti = 0.8450;
+double GBPParduoti = 0.9060;
+
+double USDBendras = 1.0713;
+double USDPirkti = 1.0547;
+double USDParduoti = 1.1309;
+
+double INRBendras = 88.8260;
+double INRPirkti = 85.2614;
+double INRParduoti = 92.8334;
+
+void ValiutosKeitimas();
+
+int main() {
+    double valiuta = 0.0;
+    return 0;
+}
+void ValiutosKeitimas();
+    double valiuta = 0.0;
+    if(valiuta == )
