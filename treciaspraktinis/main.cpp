fc9283b82c40ec36ce843a06b7ec2edf67acc26a
NojusSe <nojus.semeklis@stud.viko.lt>
2024-11-26 19:08:17 +0200

    Padariau uzduoti

--- treciaspraktinis/main.cpp
+++ treciaspraktinis/main.cpp
@@ -1,6 +1,131 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+const string LIETUVIU_ABECELE = "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ";
+
+string kartotiRakta(const string &raktas, size_t ilgis);
+string sifruotiSuAbecele(const string &tekstas, const string &raktas);
+string desifruotiSuAbecele(const string &sifras, const string &raktas);
+string sifruotiSuASCII(const string &tekstas, const string &raktas);
+string desifruotiSuASCII(const string &sifras, const string &raktas);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int pasirinkimas;
+    string tekstas, raktas, rezultatas;
+
+    do {
+
+        cout << "\nVigenere Sifro Meniu:\n";
+        cout << "1. Sifruoti/DeSifruoti naudojant lietuviu abecele\n";
+        cout << "2. Sifruoti/Desifruoti naudojant ASCII kodus\n";
+        cout << "3. Iseiti\n";
+        cout << "Pasirinkite: ";
+        cin >> pasirinkimas;
+        cin.ignore();
+
+        if (pasirinkimas == 1 || pasirinkimas == 2) {
+            cout << "Iveskite teksta: ";
+            getline(cin, tekstas);
+            cout << "Iveskite rakta: ";
+            getline(cin, raktas);
+
+            if (pasirinkimas == 1) {
+                cout << "\n-- Naudojant lietuviu abecele --\n";
+                rezultatas = sifruotiSuAbecele(tekstas, raktas);
+                cout << "Uzsifruotas tekstas: " << rezultatas << endl;
+                rezultatas = desifruotiSuAbecele(rezultatas, raktas);
+                cout << "Issifruotas tekstas: " << rezultatas << endl;
+            } else if (pasirinkimas == 2) {
+                cout << "\n-- Naudojant ASCII koduote --\n";
+                rezultatas = sifruotiSuASCII(tekstas, raktas);
+                cout << "Uzsifruotas tekstas: ";
+                for (char c : rezultatas) {
+                    cout << (int)c << " ";
+                }
+                cout << endl;
+                rezultatas = desifruotiSuASCII(rezultatas, raktas);
+                cout << "Issifruotas tekstas: " << rezultatas << endl;
+            }
+        } else if (pasirinkimas != 3) {
+            cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+        }
+    } while (pasirinkimas != 3);
+
+    cout << "Programa uzsidare.\n";
     return 0;
+}
+
+
+string kartotiRakta(const string &raktas, size_t ilgis) {
+    string pakartotasRaktas;
+    size_t raktasIlgis = raktas.length();
+
+    for (size_t i = 0; i < ilgis; i++) {
+        pakartotasRaktas += raktas[i % raktasIlgis];
+    }
+    return pakartotasRaktas;
+}
+
+
+string sifruotiSuAbecele(const string &tekstas, const string &raktas) {
+    string pakartotasRaktas = kartotiRakta(raktas, tekstas.length());
+    string sifras;
+
+    for (size_t i = 0; i < tekstas.length(); i++) {
+        size_t tekstasIndeksas = LIETUVIU_ABECELE.find(tekstas[i]);
+        size_t raktasIndeksas = LIETUVIU_ABECELE.find(raktas[i]);
+
+        if (tekstasIndeksas != string::npos && raktasIndeksas != string::npos) {
+            size_t sifroIndeksas = (tekstasIndeksas + raktasIndeksas) % LIETUVIU_ABECELE.size();
+            sifras += LIETUVIU_ABECELE[sifroIndeksas];
+        } else {
+            sifras += tekstas[i];
+        }
+    }
+    return sifras;
+}
+
+
+string desifruotiSuAbecele(const string &sifras, const string &raktas) {
+    string pakartotasRaktas = kartotiRakta(raktas, sifras.length());
+    string tekstas;
+
+    for (size_t i = 0; i < sifras.length(); i++) {
+        size_t sifroIndeksas = LIETUVIU_ABECELE.find(sifras[i]);
+        size_t raktasIndeksas = LIETUVIU_ABECELE.find(raktas[i]);
+
+        if (sifroIndeksas != string::npos && raktasIndeksas != string::npos) {
+            size_t tekstasIndeksas = (sifroIndeksas + LIETUVIU_ABECELE.size() - raktasIndeksas) % LIETUVIU_ABECELE.size();
+            tekstas += LIETUVIU_ABECELE[tekstasIndeksas];
+        } else {
+            tekstas += sifras[i]; // Neabėcėlės simboliai paliekami nepakeisti
+        }
+    }
+    return tekstas;
+}
+
+
+string sifruotiSuASCII(const string &tekstas, const string &raktas) {
+    string pakartotasRaktas = kartotiRakta(raktas, tekstas.length());
+    string sifras;
+
+    for (size_t i = 0; i < tekstas.length(); i++) {
+        char uzsifruotasSimbolis = (tekstas[i] + pakartotasRaktas[i]) % 256;
+        sifras += uzsifruotasSimbolis;
+    }
+    return sifras;
+}
+
+
+string desifruotiSuASCII(const string &sifras, const string &raktas) {
+    string pakartotasRaktas = kartotiRakta(raktas, sifras.length());
+    string tekstas;
+
+    for (size_t i = 0; i < sifras.length(); i++) {
+        char issifruotasSimbolis = (sifras[i] - pakartotasRaktas[i] + 256) % 256;
+        tekstas += issifruotasSimbolis;
+    }
+    return tekstas;
 }
